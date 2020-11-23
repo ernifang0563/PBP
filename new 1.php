1cdc684647ec9ac9f508ea6b154c0040bc34cbdf
ernifang0563 <66763368+ernifang0563@users.noreply.github.com>
2020-11-23 08:46:12 +0700

    Add files via upload

---
+++ new 1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Popup Dialog Alert - www.sistemit.com</title>
+    </head>
+    <body>
+    <script>
+        alert("Selamat datang di tutorial SISTEMIT.COM");
+    </script>
+    </body>
+</html>
